ca684605c5b761bfc1a0e4d5a854e12d8d2e5efe
Abir Dutta <abirdutta390@gmail.com>
2024-05-01 08:55:03 +0000

    added dabout page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-wxl text-center font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        veritatis temporibus a veniam quisquam delectus totam omnis adipisci,
+        esse eos, animi aliquid vero! Libero in sapiente atque, commodi
+        molestiae aut rerum? Molestiae aliquam quod et ut veniam. Enim, dolor
+        sequi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        veritatis temporibus a veniam quisquam delectus totam omnis adipisci,
+        esse eos, animi aliquid vero! Libero in sapiente atque, commodi
+        molestiae aut rerum? Molestiae aliquam quod et ut veniam. Enim, dolor
+        sequi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+        veritatis temporibus a veniam quisquam delectus totam omnis adipisci,
+        esse eos, animi aliquid vero! Libero in sapiente atque, commodi
+        molestiae aut rerum? Molestiae aliquam quod et ut veniam. Enim, dolor
+        sequi!
+      </p>
+    </div>
+  );
+}
