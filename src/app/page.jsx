9ed768331e3d19cf0ceb67f2324fc8424886edee
Abir Dutta <abirdutta390@gmail.com>
2024-05-01 09:50:21 +0000

    created axios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+const api_key = process.env.API_KEY;
 
-function Home() {
+export default async function Home() {
+  const options = {
+    method: "GET",
+    url: "https://ott-details.p.rapidapi.com/getnew",
+    params: {
+      region: "IN",
+      page: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": api_key,
+      "X-RapidAPI-Host": "ott-details.p.rapidapi.com",
+    },
+  };
+   try {
+     const response = await axios.request(options);
+     console.log(response.data);
+   } catch (error) {
+     console.error(error);
+   }
+
   return <div></div>;
 }
-
-export default Home;
